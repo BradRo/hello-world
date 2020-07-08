b25ece2b298769254374906694dbfb8a05800fa2
Brad Rooks <BradRo@users.noreply.github.com>
2020-07-07 21:58:21 -0500

    test

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print('Hello, world!')
+#print('Hello, world!')
+
+while True:
+    print('Enter your age:')
+    age = input()
+    try:
+        age = int(age)
+    except:
+        print('Please use numeric digits.')
+        continue
+    if age < 1:
+        print('Please enter a positive number.')
+        continue
+    break
+
+print(f'Your age is {age}.')
